7f9454a7a7a1558cd77dd22b3898ae128880bfd5
Ludvig Bergenstråhle <jlbergenstrahle@gmail.com>
2025-08-08 19:13:16 +0200

    stack: fix return type annotation and disallow exception supression

--- src/effects/util.py
+++ src/effects/util.py
@@ -1,35 +1,38 @@
-import sys
+from typing import Any, overload, Literal
+from types import TracebackType
 
 
-class stack:
-    def __init__(self, *context_managers, return_value=None):
+class stack[T]:
+    def __init__(self, *context_managers: Any, return_value: T | None = None) -> None:
         self._context_managers = context_managers
         self._return_value = return_value
 
-    def __enter__(self):
+    @overload
+    def __enter__(self: "stack[None]") -> "stack[None]": ...
+    
+    @overload
+    def __enter__(self: "stack[T]") -> T: ...
+    
+    def __enter__(self: "stack[None] | stack[T]"):
         for context_manager in self._context_managers:
             context_manager.__enter__()
         if self._return_value is not None:
             return self._return_value
         return self
 
-    def __exit__(self, exc_type, exc_val, exc_tb):
-        suppressed = False
+    def __exit__(
+        self,
+        exc_type: type[BaseException] | None,
+        exc_val: BaseException | None,
+        exc_tb: TracebackType | None
+    ) -> Literal[False]:
+        # Call __exit__ on all context managers in reverse order
+        # If any raise an exception, that exception will propagate
         for context_manager in reversed(self._context_managers):
-            try:
-                if context_manager.__exit__(exc_type, exc_val, exc_tb):
-                    suppressed = True
-                    # Clear exception details for subsequent handlers
-                    exc_type, exc_val, exc_tb = None, None, None
-            except Exception:
-                # A new exception in __exit__ overrides the old one
-                suppressed = False
-                exc_type, exc_val, exc_tb = sys.exc_info()
+            context_manager.__exit__(exc_type, exc_val, exc_tb)
+        return False
 
-        # Return False to propagate an unsuppressed exception, True otherwise.
-        return suppressed
-
-    def __repr__(self):
+    def __repr__(self) -> str:
         context_managers_str = ", ".join(str(cm) for cm in self._context_managers)
         return (
             f"stack(context_managers=[{context_managers_str}], "
